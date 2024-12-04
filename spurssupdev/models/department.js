878b3e8b95461b3e4444880b9cea00e86ced10bf
Harshil Shah <hashah@email.sc.edu>
2024-12-03 19:31:54 -0500

    removed everything

--- spurssupdev/models/department.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const departmentSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true }
-});
-
-module.exports = mongoose.model('Department', departmentSchema);
