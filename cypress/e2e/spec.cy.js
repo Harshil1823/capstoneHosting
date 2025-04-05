8ea0385e7001413d8bf82961ccc69b15c1c74f7c
Harshil Shah <hashah@email.sc.edu>
2025-04-05 15:52:39 -0400

    deleting files

--- cypress/e2e/spec.cy.js
+++
@@ -1,6 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    // Ensure the server is running at localhost:3000 before running the test.
-    cy.visit('http://localhost:3000/');
-  });
-});
